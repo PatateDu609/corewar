a75a1a501ee1c1c57be00b0dbe9f6f6f2c006d07
krolhm <raphaelb1@icloud.com>
2022-09-19 12:06:12 +0200

    bru

--- src/vm.h
+++ src/vm.h
@@ -3,11 +3,11 @@
 
 typedef struct	s_vm
 {
-	int nbr_cycles;
-	int nbr_champions;
-        int nchampion[MAX_PLAYERS];
-	char champion[MAX_PLAYERS];
-	char arena[MEM_SIZE];
+	int	nbr_cycles;
+	int	nbr_champions;
+        int	nchampion[MAX_PLAYERS];
+	char	champion[MAX_PLAYERS];
+	char	arena[MEM_SIZE];
 }		t_vm;
 
 
