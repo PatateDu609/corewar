846abdbad73f3879e10c23442922b9e6f6792db4
Ghali Boucetta <gboucett@student.42.fr>
2022-09-20 16:13:17 +0200

    Adding the libft and a few messages to the makefile

--- mkvars/vars.mk
+++ mkvars/vars.mk
@@ -15,11 +15,14 @@ else
 CFLAGS			+=	-O2
 endif
 
-# Maybe we'll need the libft... or another homemade library
-LDFLAGS			:=
-
 SRC_PATH		:=	src
 OBJ_PATH		:=	obj
 INC_PATH		:=	include
+LIB_PATH		:=	lib
 
+LIBFT_PATH		:=	$(LIB_PATH)/libft
+LIBFT			:=	$(LIBFT_PATH)/libft.a
+
+# Maybe we'll need the libft... or another homemade library
 CFLAGS			+=	-I$(INC_PATH)
+LDFLAGS			:= -L$(LIBFT_PATH) -lft
