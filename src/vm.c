54e159e9479ec2fc88ae532fbdfe6aae279699e2
krolhm <raphaelb1@icloud.com>
2022-09-25 16:13:59 +0200

    error parsing...

--- src/vm.c
+++ src/vm.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
-#include "op.h"
+#include <string.h>
 #include "vm.h"
+#include <fcntl.h>
+#include "../lib/libft/include/libft.h"
 
 void	init_vm(t_vm *vars)
 {
@@ -12,43 +14,81 @@ int	parsing_args(int argc, char **argv, t_vm *vars)
 {
 	for (int i = 1; i < argc; i++)
 	{
-		if (*argv[i] == "-dump")
+		if (strcmp(argv[i], "-dump") == 0)
 		{
-			vars->nbr_cycles = argv[i + 1];
+			vars->nbr_cycles = atoi(argv[i + 1]);
 		}
-		else if (*argv[i] == "-n")
+		else if (strcmp(argv[i], "-n") == 0)
 		{
+			vars->nchampion[vars->nbr_champions] = atoi(argv[i + 1]);
+			vars->champion[vars->nbr_champions] = argv[i + 2];
 			vars->nbr_champions++;
-			vars->nchampion[vars->nbr_champions] = atoi(*argv[i + 1]);
-			vars->champion[vars->nbr_champions] = *argv[i + 2];
 			i++;
 		}
 		else
 		{
-			vars->nbr_champions++;
 			vars->nchampion[vars->nbr_champions] = vars->nbr_champions;
-			vars->champion[vars->nbr_champions] = *argv[i];
+			vars->champion[vars->nbr_champions] = argv[i];
+			// printf("Champ n%d: %s !\n",vars->nbr_champions, vars->champion[vars->nbr_champions]);
+			vars->nbr_champions++;
 		}
 	}
 	return (1);
 }
 
-void	parsing_files(t_vm *vars)
+int	string_len(t_vm *vars, int i)
 {
+	char	buff[BUFF_SIZE + 1];
+	int	ret;
+	int	fd;
+
+	vars->champion_len[i] = 0;
+	if ((fd = open(vars->champion[i], O_RDONLY)) == -1)
+		printf("Error: open file %s\n", vars->champion[i]);
+	while ((ret = read(fd, buff, BUFF_SIZE)) > 0)
+	{
+		if (ret != 0)
+			vars->champion_len[i] += ret;
+	}
+	if (vars->champion_len[i] > TOTAL_SIZE)
+		printf("Error: size file too big\n");
+	if (close(fd) == -1)
+		printf("Error: closing fd\n");
+	if (ret == -1)
+		printf("Error: idk\n");
+	return (vars->champion_len[i]);
+}
+
+void	parsing_champ(t_vm *vars)
+{
+	int fd;
+	int len;
+
 	for (int i = 0; i < vars->nbr_champions; i++)
 	{
-		
+		if (strcmp(vars->champion[i] + strlen(vars->champion[i]) - 4, ".cor") != 0)
+			printf("Error: bad champ extension (%s) !\n", vars->champion[i]);
+		len = string_len(vars, i);
+		if ((fd = open(vars->champion[i], O_RDONLY)) == -1)
+			printf("Error: open file %s\n", vars->champion[i]);
+		if (read(fd, vars->champion_string[i], vars->champion_len[i]) == -1)
+			printf("Error: read\n");
+		vars->champion_string[len] = 0;
+		if (close(fd) == -1)
+			printf("Error: close\n");
 	}
 }
 
 int	main(int argc, char**argv)
 {
 	t_vm vars;
-	if (argc > CHAMP_MAX_SIZE * 3 + 2 || argc < 2 + 2 * 3) // Reverifier !!!
-		printf("Error: bad number of args\n");
+	if (argc == 1)
+		printf("Usage : corewar [-ctmo cycle_tmp_mod_out_val]\n\t[-comment] [-pourcent pourcent]\n\t[-by_step] [-aff] [-aff_shell] [-aff_proc]\n\t[-dump nbr_cycle] [-noX cycle_wait_before_affiche]\n\t[[-n prog_number] [-a load_address] prog_name] ...\n\t  Touches :\n\t\t+/- : ralenti\n\t\td   : dump mem\n\t\ts   : step by step On/Off ('space'== next cycle)\n\t\tp   : aff_shell On/Off\n\t\ta   : aff On/Off\n\t\tq   : exit");
+	// if (argc > CHAMP_MAX_SIZE * 3 + 2 || argc < 2 + 2 * 3)
+	// 	printf("Error: bad number of args\n");
 	init_vm(&vars);
 	if (!parsing_args(argc, argv, &vars))
 		printf("Error: bad args\n");
-	parsing_files(&vars);
+	parsing_champ(&vars);
 	return (0);
 }
--- src/vm.h
+++ src/vm.h
@@ -1,12 +1,18 @@
 #ifndef VM_H
 # define VM_H
 
+#include "op.h"
+
+#define TOTAL_SIZE (CHAMP_MAX_SIZE + PROG_NAME_LENGTH + COMMENT_LENGTH + 1)
+
 typedef struct	s_vm
 {
 	int	nbr_cycles;
 	int	nbr_champions;
         int	nchampion[MAX_PLAYERS];
-	char	champion[MAX_PLAYERS];
+	char*	champion[MAX_PLAYERS];
+	char*	champion_string[MAX_PLAYERS];
+	int	champion_len[MAX_PLAYERS];
 	char	arena[MEM_SIZE];
 }		t_vm;
 
